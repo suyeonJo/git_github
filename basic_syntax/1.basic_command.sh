d6dd726992f9b78bcca232636339c11af2ee4638
suyeonJo <tndus8334@naver.com>
2025-07-04 11:08:40 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -6,3 +6,10 @@ git commit -m "메시지"
 
 # 원격 repo에 main브랜치 변경사항(commitID)업로드
 git push origin main
+
+
+# git pull은 원격의 변경사항을 Local로 내려받는것(working directory까지 반영)
+git pull origin 브랜치명
+
+# 원격에 변경사항을 Local에 가져오되 병합은 하지않는것
+git fetch origin main
